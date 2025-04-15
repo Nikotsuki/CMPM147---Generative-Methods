d4801fc4dc11c187f064643a2233970744b8234f
Nikolas Otsuki <95511698+Nikotsuki@users.noreply.github.com>
2025-04-14 17:42:21 -0700

    Update sketch.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,121 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported setup, draw */
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let seed = 7;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+const leafColor = "#719552";
+const leaf2Color = "#4b6732";
+const leaf3Color = "#637e40";
+const sky1Color = "#5380c1";
+const sky2Color = "#5481c0";
+const sky3Color = "#6089c0";
+const sky4Color = "#6f93ca";
+const sky5Color = "#86a6d2";
+const sky6Color = "#b8c6d2";
+const treeColor = "#374427";
+const flowerColor = "#e8b32b";
+const insideColor = "#57470b";
+const cloudColor = "#dee5ed";
+//const cloud2Color = "#c9cdd6";
+const stemColor = "#81804d";
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+  createCanvas(400, 200);
+  createButton("reimagine").mousePressed(() => seed++);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  randomSeed(seed);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  background(100);
+
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
+  fill(sky1Color);
+  rect(0, 0, width, height);
+  
+  fill(sky2Color);
+  rect(0, height/2 - (70), width, height);
+  
+  fill(sky3Color);
+  rect(0, height/2 - (55), width, height);
+  
+  fill(sky4Color);
+  rect(0, height/2 - 25, width, height);
+  
+  fill(sky5Color);
+  rect(0, height/2, width, height);
+  
+  fill(sky6Color);
+  rect(0, height/2 + 20, width, height);
+  
+  fill(cloudColor);
+  const clouds = 20;
+  for (let i = 0; i < clouds + 1; i++) {
+    let track = (millis() / 1839.0) % 1;
+    let x = width * random() + (millis() / 5000.0) % 1;
+    let y = height*random();
+    let x2 = 50 + (10*random());
+    let y2 = 90*random()*random()*random();
+    arc(x, y, x2, y2, 0, 2*Math.PI);
+    arc(x+30*random(), y+30*random(), x2 * random() + 25, y2*random() + 30, 0, 2*Math.PI);
+    arc(x+10*random(), y+60*random(), x2 * random() + 25, y2*random() + 30, 0, 2*Math.PI);
+    //fill(cloud2Color);
+    //arc(x, y, x2 - 10, y2-10, 2*Math.PI, Math.PI);
+    //fill(cloudColor);
+  }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  
+  fill(treeColor);
+  const steps = 10;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width * i) / steps;
+    arc(x, height*0.66+5, 50 + (10*random()), (90*random() * random()), 0, 2*Math.PI, PIE, 5);
+  }
+  
+  fill(leafColor);
+  rect(0, height * 0.66+5, width, height * 0.66);
+
+  const scrub = mouseX/width;
+  const leaves = 4000;
+  for (let i = 0; i < leaves; i++) {
+    let z = random();
+    let x = width * ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+    //let s = width / 100 / z;
+    let y = height * 0.66 + height / (40 * z);
+    fill(leaf2Color);
+    arc(x, y+5, y/30, y/20, 0, 2 * Math.PI);
+  }
+  
+  
+  const leaves2 = 1500;
+  for (let i = 0; i < leaves2; i++) {
+    let z = random();
+    let x = width * ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+    //let s = width / 100 / z;
+    let y = height * 0.66 + height / (40 * z);
+    fill(leaf3Color);
+    arc(x, y+5, y/30, y/20, 0, 2 * Math.PI);
+  }
+  
+  
+  const flowers = 700 + 500*random();
+
+  for (let i = 0; i < flowers; i++) {
+    let z = random();
+    let x = width * ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+    //let s = width / 100 * z;
+    let y = height * 0.66 + height / (40* z);
+    if (y >= 190){
+      continue;
+    }
+    if (y >= 170){
+      stroke(stemColor);
+      line(x, y, x, y+60);
+      noStroke();
+    }
+    fill(flowerColor);
+    arc(x, y, y/30 + 3*random(), y/30, 180*random(), 2 * Math.PI);//flower
+    fill(insideColor);
+    arc(x, y, y/50 + 3*random(), y/50, 0, 2 * Math.PI);//inside
+  }
 }
